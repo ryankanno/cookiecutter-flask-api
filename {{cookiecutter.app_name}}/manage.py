e0bf1c8acddc62ff2b44122f8139463933df54e8
Ryan Kanno <ryankanno@localkinegrinds.com>
2015-05-18 22:03:21 -1000

    Added create/drop database manage commands

--- {{cookiecutter.app_name}}/manage.py
+++ {{cookiecutter.app_name}}/manage.py
@@ -15,6 +15,24 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+
+@manager.command
+def createdb():
+    """
+    Creates all database tables
+    """
+    db.create_all()
+
+
+@manager.command
+def dropdb():
+    """
+    Drops all database tables
+    """
+    if prompt_bool("Are you sure you want to lose all your data"):
+        db.drop_all()
+
+
 if __name__ == "__main__":
     manager.run()
 
